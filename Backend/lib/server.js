73b3884c1cf45dc325bce762e1c7ac8e6d807f7d
mosefatullah <mosefatullah@gmail.com>
2024-05-31 11:08:20 +0600

    Backend: #2 MongoDB & Project Structure

--- Backend/lib/server.js
+++ Backend/lib/server.js
@@ -3,12 +3,43 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+/*
+* Name: eDokan
+* Description: An E-Commerce Store Application
+*/
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+/* Express App Initializaion */
 const app = (0, express_1.default)();
-app.get("/time", (req, res) => {
+/* Express Middlewares */
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({
+    origin: "*",
+    methods: "GET,PUT,POST,DELETE",
+}));
+/* MongoDB Connection */
+mongoose_1.default
+    .connect(process.env.MONGODB_URI || "")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log(err));
+/* Application Routes */
+app.get("/", (req, res) => {
     const currentTime = new Date().toLocaleTimeString();
     res.send(`<!DOCTYPE html><html><head><title>Current Time</title></head><body><p>${currentTime}</p></body></html>`);
 });
+/* Error Handling */
+app.use((err, req, res, next) => {
+    if (req.headersSent) {
+        return next(err);
+    }
+    res.status(500).json({
+        error: err || "Some error occurred!",
+    });
+});
+/* Server Initialization */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 //# sourceMappingURL=server.js.map
--- Backend/server.ts
+++ Backend/server.ts
@@ -1,13 +1,51 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
+/* 
+* Name: eDokan
+* Description: An E-Commerce Store Application
+*/
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+/* Express App Initializaion */
 const app = express();
 
-app.get("/time", (req: any, res: any) => {
+/* Express Middlewares */
+app.use(express.json());
+app.use(
+ cors({
+  origin: "*",
+  methods: "GET,PUT,POST,DELETE",
+ })
+);
+
+
+/* MongoDB Connection */
+mongoose
+ .connect(process.env.MONGODB_URI || "")
+ .then(() => console.log("Connected to MongoDB"))
+ .catch((err) => console.log(err));
+
+/* Application Routes */
+app.get("/", (req: any, res: any) => {
  const currentTime = new Date().toLocaleTimeString();
  res.send(
   `<!DOCTYPE html><html><head><title>Current Time</title></head><body><p>${currentTime}</p></body></html>`
  );
 });
 
+/* Error Handling */
+app.use((err: any, req: any, res: any, next: Function) => {
+ if (req.headersSent) {
+  return next(err);
+ }
+ res.status(500).json({
+  error: err || "Some error occurred!",
+ });
+});
+
+/* Server Initialization */
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
