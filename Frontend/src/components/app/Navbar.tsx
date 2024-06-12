e48c5b8c1239e41b903e08bef58e33160f4b4ebd
mosefatullah <mosefatullah@gmail.com>
2024-06-12 17:35:09 +0600

    .

--- Frontend/src/components/app/Navbar.tsx
+++ Frontend/src/components/app/Navbar.tsx
@@ -15,9 +15,9 @@ export default function Navbar({ isSidebarOpen, setSidebar }: { isSidebarOpen: b
                            <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                         </svg>
                      </button>
-                     <NavLink to="/app" className="flex md:me-24">
+                     <NavLink to="/app" className="flex items-center md:me-24">
                         <img src="/logo-blank.png" alt="eDokan" className="h-8" />
-                        <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
+                        <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap -ml-1 dark:text-white">
                            Dokan
                         </span>
                      </NavLink>
